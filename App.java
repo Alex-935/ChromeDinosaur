3e338844787c6bfaeb2d646e3ce007b0135a3afb
Alex <69348601+Alex-935@users.noreply.github.com>
2025-11-13 08:18:33 +0000

    JFrame

--- App.java
+++ App.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
-import
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
 
+        int boardWidth = 1500;
+        int boardHeight = 500;
 
+        JFrame frame = new JFrame("Chrome Dinosaur");
+        frame.setPositionRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.CLOSE_ON_EXIT);
+
+
+        frame.setVisable();
     }
  }
